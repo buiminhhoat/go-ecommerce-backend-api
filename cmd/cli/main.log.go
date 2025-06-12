f9068c3d5a0479aeec7648af7755047bbe4fc0a6
buiminhhoat <official.buiminhhoat@gmail.com>
2025-06-12 23:10:58 +0700

    Add log

---
+++ cmd/cli/main.log.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func main() {
+	// sugar := zap.NewExample().Sugar()
+	// sugar.Infof("Hello name: %s, age: %d", "buiminhhoat", 22)
+
+	// logger := zap.NewExample()
+	// logger.Info("Hello", zap.String("name", "buiminhhoat"), zap.Int("age", 22))
+
+	// logger := zap.NewExample()
+	// logger.Info("Hello NewExample")
+
+	// logger, _ = zap.NewDevelopment()
+	// logger.Info("Hello NewDevelopment")
+
+	// logger, _ = zap.NewProduction()
+	// logger.Info("Hello NewProduction")
+
+	encoder := getEncoderLog()
+	sync := getWriterSync()
+	core := zapcore.NewCore(encoder, sync, zapcore.InfoLevel)
+
+	logger := zap.New(core, zap.AddCaller())
+
+	logger.Info("Info log", zap.Int("line", 1))
+	logger.Error("Error log", zap.Int("line", 2))
+}
+
+func getEncoderLog() zapcore.Encoder {
+	encodeConfig := zap.NewProductionEncoderConfig()
+	// 1749743899.8385055 -> 2025-06-12T22:58:19.837+0700
+	encodeConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	// ts -> Time
+	encodeConfig.TimeKey = "time"
+	// from info INFO
+	encodeConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	// "caller":"cli/main.log.go:19"
+	encodeConfig.EncodeCaller = zapcore.ShortCallerEncoder
+	return zapcore.NewJSONEncoder(encodeConfig)
+}
+
+func getWriterSync() zapcore.WriteSyncer {
+	file, _ := os.OpenFile("./log/log.txt", os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	syncFile := zapcore.AddSync(file)
+	syncConsole := zapcore.AddSync(os.Stderr)
+
+	return zapcore.NewMultiWriteSyncer(syncConsole, syncFile)
+}
