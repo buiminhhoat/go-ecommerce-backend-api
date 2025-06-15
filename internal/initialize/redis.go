e548340377dc2852e4b25576de6fa65ab9807b28
buiminhhoat <official.buiminhhoat@gmail.com>
2025-06-15 18:53:20 +0700

    Update project

--- internal/initialize/redis.go
+++ internal/initialize/redis.go
@@ -1,5 +1,25 @@
 package initialize
 
-func InitRedis() {
+import (
+	"context"
 
+	"github.com/buiminhhoat/go-ecommerce-backend-api/global"
+	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
+)
+
+var ctx = context.Background()
+
+func InitRedis() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+		PoolSize: 10,
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		global.Logger.Error("Redis initialization Error: ", zap.Error(err))
+	}
 }
