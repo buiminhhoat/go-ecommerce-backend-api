a4d1e4bfc91358a6fbdf5377d6aee5329775e25b
buiminhhoat <official.buiminhhoat@gmail.com>
2025-06-15 09:17:26 +0700

    Add middlewares

--- internal/controller/user.controller.go
+++ internal/controller/user.controller.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/buiminhhoat/go-ecommerce-backend-api/internal/service"
 	"github.com/buiminhhoat/go-ecommerce-backend-api/pkg/response"
 	"github.com/gin-gonic/gin"
@@ -17,5 +19,6 @@ func NewUserController() *UserController {
 }
 
 func (uc *UserController) GetUserById(c *gin.Context) {
+	fmt.Println("---> My Handler")
 	response.SuccessResponse(c, 20001, []string{"buiminhhoat", "Bùi Minh Hoạt"})
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,12 +1,38 @@
 package router
 
 import (
+	"fmt"
+
 	"github.com/buiminhhoat/go-ecommerce-backend-api/internal/controller"
+	"github.com/buiminhhoat/go-ecommerce-backend-api/internal/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
+func AA() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("Befor --> AA")
+		c.Next()
+		fmt.Println("Alter --> AA")
+	}
+}
+
+func BB() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("Befor --> BB")
+		c.Next()
+		fmt.Println("Alter --> BB")
+	}
+}
+
+func CC(c *gin.Context) {
+	fmt.Println("Before --> CC")
+	c.Next()
+	fmt.Println("Alter --> CC")
+}
+
 func NewRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(middlewares.AuthenMiddleware(), BB(), CC)
 
 	v1 := r.Group("/v1")
 	{
